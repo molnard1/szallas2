ce0c6cc7c14c4e7a1f8219290cd3626b46117886
molnard1 <101341428+molnard1@users.noreply.github.com>
2024-02-14 13:18:56 +0100

    Fix navbar

--- src/js/components/NavbarComponent.jsx
+++ src/js/components/NavbarComponent.jsx
@@ -11,15 +11,14 @@ export default function NavbarComponent() {
 	<>
 		<nav className="navbar navbar-expand-lg navbar-dark bg-dark">
 			<div className="container">
-				<a className="navbar-brand" href="/">Szállások</a>
+				<Link className="navbar-brand" to="/">Szállások</Link>
 				<button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
 				<span className="navbar-toggler-icon"></span>
 				</button>
 				<div className="collapse navbar-collapse" id="navbarNav">
 				<ul className="navbar-nav me-auto">
 					<li className="nav-item">
-					<a className="nav-link" href="/create">
-						Új Szállás </a>
+					<Link className="nav-link" to="/create">Új Szállás</Link>
 					</li>
 				</ul>
 				{!data ?
